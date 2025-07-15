6f26b2e49db3c26659c8d4f9d381b0f26a25abf3
David Bilson <davidbilson99@gmail.com>
2025-07-15 12:44:28 +0100

    Refactor conditional checks in Create component

--- src/pages/job/create/index.tsx
+++ src/pages/job/create/index.tsx
@@ -3,24 +3,21 @@ import React from 'react'
 import CreateJob from './_create'
 
 const Create = () => {
-
     const { userId, role } = useAuthStore()
 
     if (!userId) {
         return <div className='text-center p-6 text-red-500'>Unauthorized access!</div>
     }
 
-    if (userId && role === "contractor") {
+    if (role === 'contractor') {
         return <div className='text-center p-6 text-red-500'>Unauthorized access!</div>
     }
 
-    if (userId && role === "client") {
-        return (
-            <>
-                <CreateJob />
-            </>
-        )
+    if (role === 'client') {
+        return <CreateJob />
     }
+
+    return <div className='text-center p-6 text-red-500'>Unauthorized access!</div>
 }
 
 export default Create
